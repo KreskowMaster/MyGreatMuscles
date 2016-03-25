ccb1b64a127c91107dec7cc7c334420a65e6afee
Krystian Gontarek <krystian@gontarek.net>
2016-03-26 00:57:53 +0100

    Initial Commit

---
+++ MyGreatMuscles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGreatMuscles
+//
+//  Created by Krystian Gontarek on 26.03.2016.
+//  Copyright © 2016 KreskowDevCrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
