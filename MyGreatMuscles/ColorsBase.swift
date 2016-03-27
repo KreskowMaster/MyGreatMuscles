2927be380ce9017d4c8a7029a792956bc4038733
Krystian Gontarek <krystian@gontarek.net>
2016-03-27 01:07:12 +0100

    Created ViewControllers - Category and Timeline Issue with page control

---
+++ MyGreatMuscles/ColorsBase.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsBase.swift
+//  MyGreatMuscles
+//
+//  Created by Krystian Gontarek on 26.03.2016.
+//  Copyright © 2016 KreskowDevCrew. All rights reserved.
+//
+
+import UIKit
+
+let lightYellowOne = UIColor(hex: "FFF156")
+let lightYellowTwo = UIColor(hex: "F9E82A")
+let mainYellow = UIColor(hex: "DFCC03")
+let darkYellowOne = UIColor(hex: "B7A800")
+let darkYellowTwo = UIColor(hex: "938600")
+let blackBackgroundOne = UIColor(hex: "1F2124")
+let blackBackgroundTwo = UIColor(hex: "383A3F")
