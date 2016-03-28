a9100cc11b38d6f12e54a34d8d23a3d141c06371
Krystian Gontarek <krystian@gontarek.net>
2016-03-28 22:30:09 +0200

    Categories switched to button CategoriesViewController - Version 2 with UIButtons

--- MyGreatMuscles/CategoriesViewController.swift
+++ MyGreatMuscles/CategoriesViewController.swift
@@ -10,38 +10,23 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
     
-    // MARK: - Categories Outlets
-    
-    @IBOutlet weak var bicepsCategory: UIImageView!
-    @IBOutlet weak var legsCategory: UIImageView!
-    @IBOutlet weak var tricepsCategory: UIImageView!
-    @IBOutlet weak var chestCategory: UIImageView!
-    @IBOutlet weak var prellumCategory: UIImageView!
-    @IBOutlet weak var backCategory: UIImageView!
-    @IBOutlet weak var neckCategory: UIImageView!
-    @IBOutlet weak var shouldersCategory: UIImageView!
-    
     var musclesCategories : [UIImageView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Assigning all categories to array
-        musclesCategories = [bicepsCategory, legsCategory, tricepsCategory, chestCategory, prellumCategory, backCategory, neckCategory, shouldersCategory]
-        
-        // MARK: - Recognizing Tap of Categories
-        
-        for muscleCategory in musclesCategories {
-            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("categoryTapped"))
-            muscleCategory.userInteractionEnabled = true
-            muscleCategory.addGestureRecognizer(tapGestureRecognizer)
-        }
     }
     
     // MARK: - FUNCTION - categoryTapped
     func categoryTapped() {
         self.performSegueWithIdentifier("goToTimelineSegue", sender: self)
     }
+    
+    @IBAction func categoryTapped(sender: AnyObject) {
+        
+        self.performSegueWithIdentifier("goToTimelineSegue", sender: self)
+    }
+    
     
     
     
